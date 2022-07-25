012666819ec5919b07f0695322424101967d3474
Daniel Arango <daniel.rodas@correounivalle.edu.co>
2022-07-24 23:53:21 -0500

    Hodgman inicial.

--- Laboratorio3/Hodgman.js
+++ Laboratorio3/Hodgman.js
@@ -2,38 +2,38 @@ function isInside(p, [a, b]) {
     return (b[0] - a[0]) * (p[1] - a[1]) > (b[1] - a[1]) * (p[0] - a[0]);
 }
 
-function getEdges(polygon) {
-    let edges = [];
+function obtenerVertices(poligono) {
+    let vertices = [];
 
-    for (let i = 0; i < polygon.length; i++) {
-        let edge = [polygon[(i + polygon.length - 1) % polygon.length], polygon[i]];
+    for (let i = 0; i < poligono.length; i++) {
+        let vertice = [poligono[(i + poligono.length - 1) % poligono.length], poligono[i]];
 
-        edges.push(edge);
+        vertices.push(edge);
     }
 
-    return edges;
+    return vertices;
 }
 
 
-function lineIntersection(line0, line1) {
+function interseccionLineas(linea0, linea1) {
     const
-        [x1, y1] = line0[0],
-        [x2, y2] = line0[1],
-        [x3, y3] = line1[0],
-        [x4, y4] = line1[1],
-        denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+        [x1, y1] = linea0[0],
+        [x2, y2] = linea0[1],
+        [x3, y3] = linea1[0],
+        [x4, y4] = linea1[1],
+        denominador = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 
-    if (denominator === 0) {
+    if (denominador === 0) {
         // Línea paralela
         return false;
     }
 
     const
-        numeratorX = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4),
-        numeratorY = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
+        numeradorX = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4),
+        numeradorY = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
 
-    let x = numeratorX / denominator,
-        y = numeratorY / denominator;
+    let x = numeradorX / denominador,
+        y = numeradorY / denominador;
 
 
     return [x, y];
@@ -42,18 +42,18 @@ function lineIntersection(line0, line1) {
 
 export default function (clipPolygon, subjectPolygon) {
     let result = subjectPolygon;
-    getEdges(clipPolygon).forEach((clipEdge) => {
+    obtenerVertices(clipPolygon).forEach((clipEdge) => {
         let pointList = result;
         result = [];
 
-        getEdges(pointList).forEach((pointEdge) => {
+        obtenerVertices(pointList).forEach((pointEdge) => {
             if (isInside(pointEdge[1], clipEdge)) {
                 if (!isInside(pointEdge[0], clipEdge)) {
-                    result.push(lineIntersection(pointEdge, clipEdge));
+                    result.push(interseccionLineas(pointEdge, clipEdge));
                 }
                 result.push(pointEdge[1]);
             } else if (isInside(pointEdge[0], clipEdge)) {
-                result.push(lineIntersection(pointEdge, clipEdge));
+                result.push(interseccionLineas(pointEdge, clipEdge));
             }
         });
     });
